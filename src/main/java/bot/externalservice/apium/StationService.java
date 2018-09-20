19c6c7d7bfa0fdc8c4cc0cbf20f0918e9a9a3c03
Drzamich <drzamich@gmail.com>
2018-09-20 14:01:01 +0200

    Links to api

--- src/main/java/bot/externalservice/apium/StationService.java
+++ src/main/java/bot/externalservice/apium/StationService.java
@@ -1,18 +1,23 @@
 package bot.externalservice.apium;
 
 import bot.externalservice.apium.data.DataManager;
+import bot.externalservice.apium.data.Platform;
 import bot.externalservice.apium.data.Station;
 import bot.processor.Utilities;
 import org.springframework.stereotype.Service;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class StationService extends DataManager {
     private Station station;
+    private String stationName;
+    private String stationId;
+    private List<Platform> platforms;
 
     public StationService() {
         this.stationsMap = deserializeStationMap();
@@ -33,7 +38,27 @@ public class StationService extends DataManager {
     }
 
     private void processStation(){
+        this.stationName = this.station.getMainName();
+        this.stationId = this.station.getId();
+        this.platforms = this.station.getPlatforms();
+        for(Platform platform: this.platforms){
+            this.processPlatform(platform);
+        }
         System.out.println(this.station);
+    }
+
+    private void processPlatform(Platform platform){
+        String platformNumber = platform.getNumber();
+        List<String> lines = platform.getLines();
+        for (String line: lines){
+            this.processLine(platformNumber,line);
+        }
+    }
+
+    private void processLine(String platformNumber,String line){
+        String urlToApi = "https://api.um.warszawa.pl/api/action/dbtimetable_get/?id=e923fa0e-d96c-43f9-ae6e-60518c9f3238"+
+         "&busstopId="+this.stationId+"&busstopNr="+platformNumber+"&line="+line+"&apikey="+Properties.API_KEY;
+        System.out.println(urlToApi);
     }
 
     private Optional<Station> findStation(String proposedName) {
