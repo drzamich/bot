40bb2a7b47dd1fad463ca8c12dd65301853205f9
Michal Drzazga <mdrzazga@microstrategy.com>
2019-01-25 09:19:44 +0100

    Fix time error

--- src/main/java/bot/Application.java
+++ src/main/java/bot/Application.java
@@ -9,10 +9,10 @@ public class Application {
 
     public static void main(String[] args) {
 
-        int devMode = 0;
+        int devMode = 1;
 
         if (devMode == 1) {
-            String msg = "centrum centralny";
+            String msg = "muranow 04";
             QueryProcessor queryProcessor = new QueryProcessor();
             queryProcessor.processQuery(msg);
             System.out.println(queryProcessor.getResponse().getInfo());
---
+++ src/main/java/bot/externalservice/apium/Properties.java
@@ -0,0 +1,29 @@
+package bot.externalservice.apium;
+
+import bot.processor.Utilities;
+import bot.schema.Station;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Properties {
+    protected Map<String, Station> stationsMap = new HashMap<>();
+    protected final String TIME_PATTERN = "HH:mm:ss";
+    protected final String PATH_TO_OBJECTS = "src/main/java/bot/externalservice/apium/data/serialized/";
+    public String time;
+    public String date;
+    public String pathToStationMap;
+    public String pathToStationList;
+    public List<String> BLOCKED_STOPS = Arrays.asList("Wyścigi 08", "Pl.Małachowskiego 02", "Metro Młociny 20", "Tor Służewiec 01");
+
+    protected static final String API_KEY = Secret.API_KEY;
+
+    public Properties(){
+        this.time = Utilities.getTime(TIME_PATTERN);
+        this.date = Utilities.getTime("yyyyMMdd");
+        this.pathToStationMap = PATH_TO_OBJECTS+this.date+"_statonMap";
+        this.pathToStationList = PATH_TO_OBJECTS+this.date+"_stationList";
+    }
+}
